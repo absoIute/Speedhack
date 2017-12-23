9a34bbd1ec2b59cbef6de2701399988f74331a4a
absolute-gamer <ben.stafford.03@icloud.com>
2017-12-23 17:38:52 +0000

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+//example of usage
+
+#include "speedhack.hpp"
+
+DWORD WINAPI MainThread(LPVOID lpParam)
+{
+	Speedhack::Setup();
+	Speedhack::Attach();
+
+	Speedhack::speed = 0.5; //slows down to half speed
+
+	return 0;
+}
+
+BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+	if (dwReason == DLL_PROCESS_ATTACH)
+	{
+		CreateThread(0, 0x1000, &MainThread, NULL, 0, NULL);
+	}
+
+	return TRUE;
+}
